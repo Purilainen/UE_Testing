ae9d4ccd8d8049d18decabf75507553f3a6d2fa6
Purilainen <p.kosola@gmail.com>
2021-03-09 11:37:08 +0200

    Initial commit, Blank UE4.26.1 Project & some C++ files to test EditorUtilityWidgets

---
+++ Source/UE_Tinkering/BasicCpp.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class UE_TINKERING_API BasicCpp
+{
+public:
+	BasicCpp();
+	~BasicCpp();
+};
